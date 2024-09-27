16b52ac121384d704dc3b3daf8f4ec5727250dd2
demonsjostle <demons.jostle@gmail.com>
2024-09-27 16:38:12 +0700

    update frontend

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -21,7 +21,7 @@ const index = () => {
     <MainLayout>
       <div className="flex flex-col items-center h-screen mt-20">
         {/* Image container */}
-        <h1 className="text-4xl mb-6">โปรโมชั่น</h1>
+        {/* <h1 className="text-4xl mb-6">โปรโมชั่น</h1> */}
         <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
           <img
             src={Promotion1}
